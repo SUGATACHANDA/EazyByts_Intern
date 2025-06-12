0dcdf6765e625434d8f2006e021a9417b2abe65c
SUGATACHANDA <scbabai2704@gmail.com>
2025-06-12 21:55:50 +0530

    fix: Refactor CORS configuration to use a dedicated options object and allow localhost access

--- server/api/index.js
+++ server/api/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors');
 const authRoutes = require('../routes/auth')
 const contactRoutes = require('../routes/contactRoutes.js');
 const skillRoutes = require("../routes/skillRoutes.js")
 const funFactRoutes = require('../routes/funFactRoutes.js');
 const projectRoutes = require('../routes/projectRoutes.js');
-
 dotenv.config();
 
+const cors = require('cors');
+
+const corsOptions = {
+    // origin: 'https://tigsimportal.vercel.app',
+    origin: 'http://localhost:5173/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allow cookies and credentials
+    allowedHeaders: ['Content-Type', 'Authorization'], // explicitly allow headers
+    optionsSuccessStatus: 204 // some legacy browsers choke on 204
+};
+
+
 const app = express();
-app.use(cors({ origin: 'https://sugata-chanda-backend.vercel.app/' }));
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -27,6 +37,18 @@ app.use('/api/funfacts', funFactRoutes);
 app.use('/api/projects', projectRoutes);
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.use((req, res, next) => {
+    // res.header("Access-Control-Allow-Origin", "https://tigsimportal.vercel.app");    
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        res.sendStatus(204); // Respond to preflight requests
+    } else {
+        next();
+    }
 });
 
 const PORT = process.env.PORT || 5000;
